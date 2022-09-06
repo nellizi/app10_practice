74c83534fdfd747ff8ef3778a281d991add13313
nellizi <masterliz9095@gmail.com>
2022-09-06 13:54:03 +0900

    업로드한 파일을 웹에서 열어보기

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -5,11 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     @RequestMapping("/")
     public String main() {
         return "home/main";
+    }
+
+    @RequestMapping("/test/upload")
+    public String upload() {
+        return "home/test/upload";
     }
 }
